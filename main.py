cff63158b4a08956e6fb16afa015c87959362556
tmdrbpp123 <tmdrbpp123@gbit.kr>
2024-03-08 15:27:57 +0900

    updated

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Decembaek_library",
+    page_icon="images\Original.png",
+    layout="centered",
+)
+st.page_link("main.py", label="Home", icon="🏠")
+
 st.title("Decembaek Library")
-col1, col2 = st.columns(2)
+
+# 명함 카드 ------
+card_container = st.container(border=True)
+col1, col2, col3 = card_container.columns([0.5, 0.8, 0.1])
 
 col1.image(
     "images/circle_profile.png",
-    caption="Profile",
-    width=50,
+    caption="",
+    width=130,
     # use_column_width="always",
 )
-col2.write("Hellow my name is Seunggyu")
+col1.subheader("")
+col1.subheader("Decembaek")
+
+col2.subheader("백승규 Seunggyu Baek")
+col2.write("경력 1년 Python 백엔드 개발자")
+col2.write("Email : tmdrbpp123@gmail.com")
+col2.write("SKill : Python, Django, deploy, Streamlit, Dart, Flutter")
+github, blog, _ = col2.columns([6, 6, 8])
+github.link_button(
+    "Go to Blog", "https://wingyu-story.tistory.com/", help="블로그 이동"
+)
+blog.link_button("Go to Blog", "https://wingyu-story.tistory.com/", help="블로그 이동")
+# 명함 카드 ------
